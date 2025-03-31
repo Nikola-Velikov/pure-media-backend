fc865d179d80848ad7ec1343068ba22843b05a76
Nikola Velikov <niki.velikov09@gmail.com>
2025-03-31 21:30:31 +0300

    add new news
    Signed-off-by: Nikola Velikov <niki.velikov09@gmail.com>

--- index.js
+++ index.js
@@ -38,6 +38,31 @@ async function start() {
    // app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
    app.listen(4000, () => { console.log('App listening on port 3000') });
 
+//    async function updateNewsCreatedAt() {
+//     try {
+//         const newsItems = await News.find(); // Fetch all news
+
+//         for (const news of newsItems) {
+//             const createdAt = new Date(news.createdAt);
+            
+//             // Check if the date is March 26, 2024
+//             if (createdAt.getFullYear() === 2025 && createdAt.getMonth() === 2 && createdAt.getDate() === 26) {
+//                 // Set createdAt to March 21, 2024
+//                 news.createdAt = new Date(2025, 2, 21); // Month is 0-based (2 = March)
+//                 await news.save(); // Save the updated news
+//                 console.log(`✅ Updated createdAt from March 26 to March 21 for: ${news.title}`);
+//             }
+//         }
+
+//         console.log("✅ Finished checking and updating news timestamps.");
+//     } catch (err) {
+//         console.error("❌ Error updating news:", err);
+//     }
+// }
+
+// // Run the function
+// updateNewsCreatedAt();
+
     //await Matches.deleteMany({});
    //  await uploadData()
   
@@ -82,7 +107,7 @@ async function start() {
 //                         });
                         
                     
-//                       // await newMatch.save();
+//                        await newMatch.save();
 //                     }else{
 //                        // console.log(matchNews);
                         
@@ -90,67 +115,90 @@ async function start() {
 //                 }
 //             }
 //         }
-//     async function saveNews(title, description, mediaName, image_url) {
-//         try {
-//             const news = new News({
-//                 image_url: image_url,
-//                 title: title,
-//                 description: description,
-//                 media: mediaName,
-//                 createdAt: new Date(new Date().setDate(new Date().getDate() - 1)) // Set the createdAt to the previous day
-//             });
-    
-//             await news.save();
-//             console.log(`Saved news: ${title}`);
-//         } catch (err) {
-//             console.error('Error saving news:', err);
+
+// const deleteNewsById = async (id) => {
+//     try {
+//         const deletedNews = await News.findByIdAndDelete(id);
+//         if (!deletedNews) {
+//              console.log(
+//               { success: false, message: "News entity not found" });
 //         }
+//         console.log( { success: true, message: "News entity deleted successfully" });
+//     } catch (error) {
+//        console.log( {success: false, message: "Error deleting news entity", error: error.message} );
 //     }
+// };
+
+// deleteNewsById("67e5aa4b71b8bd0d3c0edd40")
+
+
+    // async function saveNews(title, description, mediaName, image_url) {
+    //     try {
+    //         const existingNews = await News.findOne({ title: title });
+    //     if (existingNews) {
+           
+    //         console.log(`Duplicate news detected: ${title}`);
+    //         return; // Skip saving if it already exists
+    //     }
+    //         const news = new News({
+    //             image_url: image_url,
+    //             title: title,
+    //             description: description,
+    //             media: mediaName,
+    //             createdAt: new Date(new Date().setDate(new Date().getDate() - 1)) // Set the createdAt to the previous day
+    //         });
     
-//     // Read the file and parse news
-//     function parseNewsFile(filePath, mediaName) {
-//         fs.readFile(filePath, 'utf8', async (err, data) => {
-//             if (err) {
-//                 console.error('Error reading the file:', err);
-//                 return;
-//             }
+    //         await news.save();
+    //         console.log(`Saved news: ${title}`);
+    //     } catch (err) {
+    //         console.error('Error saving news:', err);
+    //     }
+    // }
     
-//             // Split the file content by "Title"
-//             const newsItems = data.split('Title');
+    // // Read the file and parse news
+    // function parseNewsFile(filePath, mediaName) {
+    //     fs.readFile(filePath, 'utf8', async (err, data) => {
+    //         if (err) {
+    //             console.error('Error reading the file:', err);
+    //             return;
+    //         }
+    
+    //         // Split the file content by "Title"
+    //         const newsItems = data.split('Title');
             
-//             // Process each news item
-//             for (const item of newsItems) {
-//                 // Trim extra spaces and check if item is valid
-//                 const trimmedItem = item.trim();
-//                 if (trimmedItem.length === 0) continue;
+    //         // Process each news item
+    //         for (const item of newsItems) {
+    //             // Trim extra spaces and check if item is valid
+    //             const trimmedItem = item.trim();
+    //             if (trimmedItem.length === 0) continue;
     
-//                 // Find the position of the colon (:) to separate title and description
-//                 const colonIndex = trimmedItem.indexOf(':');
-//                 if (colonIndex !== -1) {
-//                     const firstTitle = trimmedItem.substring(0, colonIndex).trim();
-//                     let title = "";
-//                     let image_url = "";
-//                     if (firstTitle.includes(";;;")) {
-//                         const splitTitle = firstTitle.split(";;;");
-//                         title = splitTitle[0]
-//                         image_url = splitTitle[1]
-//                     }else{
-//                         title = firstTitle
-//                     }
-//                     const description = trimmedItem.substring(colonIndex + 1).trim();
+    //             // Find the position of the colon (:) to separate title and description
+    //             const colonIndex = trimmedItem.indexOf(':');
+    //             if (colonIndex !== -1) {
+    //                 const firstTitle = trimmedItem.substring(0, colonIndex).trim();
+    //                 let title = "";
+    //                 let image_url = "";
+    //                 if (firstTitle.includes(";;;")) {
+    //                     const splitTitle = firstTitle.split(";;;");
+    //                     title = splitTitle[0]
+    //                     image_url = splitTitle[1]
+    //                 }else{
+    //                     title = firstTitle
+    //                 }
+    //                 const description = trimmedItem.substring(colonIndex + 1).trim();
     
-//                     // Save the news to the database
-//                     await saveNews(title, description, mediaName, image_url);
-//                 }
-//             }
+    //                 // Save the news to the database
+    //                 await saveNews(title, description, mediaName, image_url);
+    //             }
+    //         }
     
-//             console.log('All news items have been processed.');
-//         });
-//     }
+    //         console.log('All news items have been processed.');
+    //     });
+    // }
     
-//     const filePath = path.join(__dirname, 'all_news.txt');
+    // const filePath = path.join(__dirname, 'news3.txt');
     
-//     const mediaName = '24chasa';
+    // const mediaName = 'Mediapool';
     
-//     parseNewsFile(filePath, mediaName);
+    // parseNewsFile(filePath, mediaName);
  }
